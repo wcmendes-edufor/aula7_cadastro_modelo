8e1e044f3d60e6aa8c1eac57d1b50fbce56c6030
William Corrêa Mendes <william.mendes@edufor.edu.br>
2026-03-19 20:29:46 -0300

    Original

--- script.js
+++ script.js
@@ -35,17 +35,14 @@ form.addEventListener("submit", function(e) {
   mensagem.textContent = "Enviando..."
   mensagem.style.color = "black"
 
-  let idade = 42
-
-  fetch("http://servlet-data-hub.replit.app/api/cadastro", {
+  fetch("https://jsonplaceholder.typicode.com/posts", {
     method: "POST",
     headers: {
       "Content-Type": "application/json"
     },
     body: JSON.stringify({
       nome,
-      email,
-      idade
+      email
     })
   })
   .then(res => res.json())
